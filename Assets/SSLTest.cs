6c7bd85be1b2ad1775268de20a28cfdb9cadfbf7
Kasey McKenna (macbook) <kasey@freerangegames.com>
2019-09-05 15:33:12 -0700

    [ui] Add label and copy paste utility

--- Assets/SSLTest.cs
+++ Assets/SSLTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -6,29 +7,84 @@ using UnityEngine.UI;
 
 public class SSLTest : MonoBehaviour
 {
-    public string url = "https://www.twitch.tv";
-    // Start is called before the first frame update
-    void Start()
+    public GameObject CopiedAlert;
+    public Text URLLabel;
+    public int CharacterLimit = 3000;
+
+    string _cachedResult;
+    Text _cachedText;
+
+    private void Awake()
     {
-        StartCoroutine(Coroutine());
+        _cachedText = GetComponent<Text>();
+        Refresh();
     }
 
-    IEnumerator Coroutine()
+    // Start is called before the first frame update
+    public void Run(InputField field)
     {
+        StartCoroutine(Coroutine(field.text));
+    }
+
+    void Refresh(string newText = "")
+    {
+        CopiedAlert?.SetActive(false);
+        _cachedResult = null;
+        _cachedText.color = Color.white;
+        _cachedText.text = newText;
+        if (URLLabel) URLLabel.text = string.Empty;
+    }
+
+    IEnumerator Coroutine(string url)
+    {
+        Refresh($"Sending Request to {url}...");
+
         UnityWebRequest req = UnityWebRequest.Get(url);
         var response = req.SendWebRequest();
         yield return response;
-        var text = GetComponent<Text>();
-        if (req.isNetworkError)
+        try
         {
-            text.text = "Error: " + req.error;
+            if (req.isNetworkError)
+            {
+                _cachedText.color = Color.red;
+                _cachedText.text = "Error: " + req.error;
+                _cachedResult = req.error;
+            }
+            else
+            {
+                string responseText = req.downloadHandler.text;
+                if (responseText.Length > CharacterLimit)
+                    responseText = responseText.Substring(0, CharacterLimit);
+                _cachedText.text = responseText;
+                _cachedResult = responseText;
+            }
         }
-        else
+        catch(Exception e)
         {
-            string responseText = req.downloadHandler.text;
-            if (responseText.Length > 5000)
-                responseText = responseText.Substring(0, 5000);
-            text.text = responseText;
+            Debug.LogException(e);
+            _cachedText.color = Color.red;
+            _cachedText.text = "Exception: " + e.Message;
         }
+        finally
+        {
+            if(URLLabel) URLLabel.text = url;
+        }
+    }
+
+    public void CopyResultToClipboard()
+    {
+        if(_cachedResult == null)
+        {
+            Debug.LogError("No result to copy");
+            return;
+        }
+
+        TextEditor tempEditor = new TextEditor
+        {
+            text = _cachedResult
+        };
+        tempEditor.SelectAll();
+        tempEditor.Copy();
+        CopiedAlert?.SetActive(true);
     }
 }
